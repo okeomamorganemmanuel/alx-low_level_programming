4d290e770634e97151050d6ef616e755a3917e94
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2022-10-14 21:42:56 +0000

    low level language

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,4 +2,20 @@
 /**
  * main - Entry point
  *
- * Return: Always retu
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	if (a == 'q' || a == 'e')
+	{
+	continue;
+	}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
