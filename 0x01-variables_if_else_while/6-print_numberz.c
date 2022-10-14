65ae42113963ecb3e80b9316a125e8804f337833
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2022-10-14 16:09:47 -0700

    low level language

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * return: always return 0 (success)
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int x = 0;
+	int num;
 
-	for (; x < 10 ; x++)
-		putchar('0' + x);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
+
 	return (0);
-
 }
-
