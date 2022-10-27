479b9089470838398a2f9fd897b3a8fbd3316043
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2022-10-27 03:20:27 -0700

    more pointers- functions that concatenates

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: source array to concat
+ * Return: char value
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
