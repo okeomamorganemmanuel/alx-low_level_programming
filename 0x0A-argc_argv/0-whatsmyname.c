7c37991e11b9affd2c0584ab7546ac1b5694c10c
okeomamorganemmanuel <okeomamorganemmanuel@gmail.com>
2022-11-04 11:11:21 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
-* main - this a function to prints its name
-*@argc: argc parameter
-*@argv: an array of command listed
-*Return: 0 for success
-*/
-int main(int argc, *argv[])
+ * main -> this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	_putchar(*argv);
-	return(0);
+	printf("%s\n", *argv);
+	return (0);
 }
